e825ca31fa5996fa96a17378bf3e77071bb72684
Cassandra Culkins and David Remington <david.ian.remington@gmail.com>
2016-02-09 11:38:51 -0800

    add string arrays

--- src/main/java/Numbers.java
+++ src/main/java/Numbers.java
@@ -8,5 +8,40 @@ import java.util.ArrayList;
 public class Numbers {
   public static void main(String[] args) {
 
-}
+  }
+  public static String numberConverter(Integer userInput) {
+    private static final String[] lowNum = {
+      " one",
+      " two",
+      " three",
+      " four",
+      " five",
+      " six",
+      " seven",
+      " eight",
+      " nine",
+      " ten",
+      " eleven",
+      " twelve",
+      " thirteen",
+      " fourteen",
+      " fifteen",
+      " sixteen",
+      " seventeen",
+      " eighteen",
+      " nineteen"
+    };
+
+    private static final String[] tenNum = {
+      " twenty",
+      " thirty",
+      " fourty",
+      " fifty",
+      " sixty",
+      " seventy",
+      " eighty",
+      " ninety"
+
+  };
+
 }
